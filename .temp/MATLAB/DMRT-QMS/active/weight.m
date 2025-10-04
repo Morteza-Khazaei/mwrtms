0afa95f9688db47d78234bb1b1ea5ff0b34c584b
Morteza Khazaei <mortezakhazaei1370@gmail.com>
2025-10-04 00:38:30 -0600

    Stop tracking .temp directory

--- .temp/MATLAB/DMRT-QMS/active/weight.m
+++
@@ -1,8 +0,0 @@
-function vec = weight(vec0,aa,s)
-% weight of vector, 
-% assert(): length(aa)*s = length(vec0)
-vec = vec0;
-for i = 1:length(aa)
-    vec((i - 1)*s + 1:i*s) = vec((i - 1)*s + 1:i*s)*aa(i);
-end
-end
